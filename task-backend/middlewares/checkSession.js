4913fa01cd569cd944a25525831fcd39cd4c9633
husnain javed <hjchoudry@gmail.com>
2020-12-19 12:28:23 +0500

    Backend removed

--- task-backend/middlewares/checkSession.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-module.exports = (req, res, next) => {
-  if(req.cookies.user_sid && req.session.user) {
-    return res.redirect("/");
-  } else {
-    next();
-  }
-};
