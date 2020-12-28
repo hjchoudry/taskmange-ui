4f9cd75d12330c79152c52638f644470609370cd
husnain javed <hjchoudry@gmail.com>
2020-12-28 12:17:33 +0500

    make it better

--- src/config/apiBase.js
+++ src/config/apiBase.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 //Change this url for all apis
-export const baseUrl = "http://localhost:8000";
+// export const baseUrl = "http://localhost:8000";
+
+export const baseUrl = "https://taskmanger-backend.herokuapp.com";
 
 export default axios.create({
   baseURL: `${baseUrl}`
